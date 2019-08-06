682610b5e041cea4c6edf96d82cfe1d6e133f6a3
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-06 15:15:41 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,5 @@
-var playlist = {"Norah Jones": "Don't Know Why"}
+var playlist = {"Norah Jones" : "Don't Know Why"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-playlist[artistName]= songTitle
-return playlist }
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
+return  Object.assign({},{"artistName": "songTitle"})
 }
